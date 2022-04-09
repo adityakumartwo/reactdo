686fecce9f09387d413fd43c0ce76065e075e25d
adityakumartwo <adityakumartwo@gmail.com>
2022-04-09 14:04:36 +0530

    Updates

---
+++ precache-manifest.e1b94bfa7865afe081df7107dbbe3bf2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa844e2ef09be668a90f",
+    "url": "/react-tutorial/static/css/main.0c9693d8.chunk.css"
+  },
+  {
+    "revision": "fa844e2ef09be668a90f",
+    "url": "/react-tutorial/static/js/main.fa844e2e.chunk.js"
+  },
+  {
+    "revision": "db20f86d0530543db744",
+    "url": "/react-tutorial/static/js/1.db20f86d.chunk.js"
+  },
+  {
+    "revision": "3a2e0e0b763789a1abbb",
+    "url": "/react-tutorial/static/js/2.3a2e0e0b.chunk.js"
+  },
+  {
+    "revision": "ab4e3affaa2cf24bf5fc",
+    "url": "/react-tutorial/static/js/runtime~main.ab4e3aff.js"
+  },
+  {
+    "revision": "de9dbb709482ca051b791530c74ead91",
+    "url": "/react-tutorial/index.html"
+  }
+];
